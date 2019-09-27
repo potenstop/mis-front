1536f165ce3d495d1a47e6c91bde2a1750149868
potenstop <potenstop@163.com>
2019-09-27 19:21:05 +0800

    增加编辑器逻辑

--- src/components/editor/RichTextEditor.vue
+++ src/components/editor/RichTextEditor.vue
@@ -11,6 +11,7 @@
       action="//jsonplaceholder.typicode.com/posts/"
       :on-success="insertImage"
       style="display:none">
+      <Button icon="ios-cloud-upload-outline">Upload files</Button>
     </Upload>
   </div>
 </template>
@@ -53,10 +54,8 @@ import 'tinymce/plugins/pagebreak'
 import 'tinymce/plugins/paste'
 import 'tinymce/plugins/preview'
 import 'tinymce/plugins/print'
-import 'tinymce/plugins/quickbars'
 import 'tinymce/plugins/save'
 import 'tinymce/plugins/searchreplace'
-import 'tinymce/plugins/spellchecker'
 import 'tinymce/plugins/tabfocus'
 import 'tinymce/plugins/table'
 import 'tinymce/plugins/template'
@@ -77,7 +76,7 @@ export default class RichTextEditor extends Vue {
   private tinymceInit = {};
   private content = '本地图片上传功能仅为演示，实际使用需要补全图片存储地址'
 
-  private insertImage () {
+  private insertImage (value) {
     let src = '' // 图片存储地址
     tinymce.execCommand('mceInsertContent', false, `<img src=${src}>`)
   }
@@ -87,66 +86,66 @@ export default class RichTextEditor extends Vue {
       skin_url: '/tinymce/skins/ui/oxide',
       language_url: '/tinymce/langs/zh_CN.js',
       language: 'zh_CN',
-      height: 500,
-      browser_spellcheck: true, // 拼写检查
-      branding: true, // 去水印
-      elementpath: false, // 禁用编辑器底部的状态栏
-      statusbar: false, // 隐藏编辑器底部的状态栏
-      paste_data_images: true, // 允许粘贴图像
-      menubar: true, // 隐藏最上方menu
-      plugins: 'advlist anchor autolink autoresize autosave bbcode charmap code codesample colorpicker contextmenu directionality emoticons fullpage help hr image imagetools importcss insertdatetime legacyoutput link lists media nonbreaking noneditable pagebreak paste preview print quickbars save searchreplace spellchecker tabfocus table template textcolor textpattern toc visualblocks visualchars wordcount',
-      toolbar: 'code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat |table image media charmap emoticons hr pagebreak insertdatetime print preview | fullscreen | bdmap indent2em',
-      fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
+      inline: false,
+      plugins: 'advlist anchor autolink autoresize autosave bbcode charmap code codesample colorpicker contextmenu directionality emoticons fullpage help hr image imagetools importcss insertdatetime legacyoutput link lists media nonbreaking noneditable pagebreak paste preview print save searchreplace tabfocus table template textcolor textpattern toc visualblocks visualchars wordcount',
+      toolbar: 'code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat |table image media charmap emoticons hr pagebreak insertdatetime print preview | fullscreen | bdmap indent2em',
+      // toolbar: 'formatselect | bold italic strikethrough forecolor backcolor permanentpen formatpainter removeformat | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | link image media uploadbtn',
       font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;知乎配置=BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;小米配置=Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif',
-      link_list: [
-        { title: '预置链接1', value: 'http://www.tinymce.com' },
-        { title: '预置链接2', value: 'http://tinymce.ax-z.cn' }
-      ],
-      image_list: [
-        { title: '预置图片1', value: 'https://www.tiny.cloud/images/glyph-tinymce@2x.png' },
-        { title: '预置图片2', value: 'https://www.baidu.com/img/bd_logo1.png' }
-      ],
-      image_class_list: [
-        { title: 'None', value: '' },
-        { title: 'Some class', value: 'class-name' }
-      ],
-      importcss_append: true,
-      // 自定义文件选择器的回调内容
-      file_picker_callback: (resolve, value, meta) => {
-        if (meta.filetype === 'file') {
-          resolve(value)
+      fontsize_formats: '8px 10px 11px 12px 14px 18px 24px 36px',
+      style_formats_merge: true,
+      style_formats: [
+        {
+          title: '行间距',
+          inline: 'span',
+          items: [
+            { title: '1倍', inline: 'span', styles: { 'line-height': '1' } },
+            { title: '1.5倍', inline: 'span', styles: { 'line-height': '1.5' } },
+            { title: '2倍', inline: 'span', styles: { 'line-height': '2' } },
+            { title: '2.5倍', inline: 'span', styles: { 'line-height': '2.5' } },
+            { title: '3倍', inline: 'span', styles: { 'line-height': '3' } }
+          ]
+        },
+        {
+          title: '字间距',
+          inline: 'span',
+          items: [
+            { title: '0.1px', inline: 'span', styles: { 'letter-spacing': '0.1px' } },
+            { title: '0.2px', inline: 'span', styles: { 'letter-spacing': '0.2px' } },
+            { title: '0.3px', inline: 'span', styles: { 'letter-spacing': '0.3px' } },
+            { title: '0.4px', inline: 'span', styles: { 'letter-spacing': '0.4px' } },
+            { title: '0.5px', inline: 'span', styles: { 'letter-spacing': '0.5px' } },
+            { title: '0.6px', inline: 'span', styles: { 'letter-spacing': '0.6px' } },
+            { title: '0.7px', inline: 'span', styles: { 'letter-spacing': '0.7px' } },
+            { title: '0.8px', inline: 'span', styles: { 'letter-spacing': '0.8px' } },
+            { title: '0.9px', inline: 'span', styles: { 'letter-spacing': '0.9px' } },
+            { title: '1px', inline: 'span', styles: { 'letter-spacing': '1px' } },
+            { title: '1.5px', inline: 'span', styles: { 'letter-spacing': '1.5px' } },
+            { title: '2px', inline: 'span', styles: { 'letter-spacing': '2px' } },
+            { title: '3px', inline: 'span', styles: { 'letter-spacing': '3px' } },
+            { title: '4px', inline: 'span', styles: { 'letter-spacing': '4px' } },
+            { title: '5px', inline: 'span', styles: { 'letter-spacing': '5px' } }
+          ]
+        },
+        {
+          title: '两端缩进',
+          inline: 'span',
+          items: [
+            { title: '取消', inline: 'span', styles: { 'display': 'inline-block', 'padding': '0', 'word-break': 'break-word' } },
+            { title: '8px', inline: 'span', styles: { 'display': 'inline-block', 'padding-left': '8px', 'padding-right': '8px', 'word-break': 'break-word' } },
+            { title: '16px', inline: 'span', styles: { 'display': 'inline-block', 'padding-left': '16px', 'padding-right': '16px', 'word-break': 'break-word' } },
+            { title: '32px', inline: 'span', styles: { 'display': 'inline-block', 'padding-left': '32px', 'padding-right': '32px', 'word-break': 'break-word' } },
+            { title: '48px', inline: 'span', styles: { 'display': 'inline-block', 'padding-left': '48px', 'padding-right': '48px', 'word-break': 'break-word' } }
+          ]
         }
-        if (meta.filetype === 'image') {
-          resolve(value)
-        }
-        if (meta.filetype === 'media') {
-          resolve(value)
-        }
-      },
-      // 为内容模板插件提供预置模板
-      templates: [
-        { title: '模板1', description: '介绍文字1', content: '模板内容' },
-        { title: '模板2', description: '介绍文字2', content: '<div class="mceTmpl"><span class="cdate">CDATE</span>，<span class="mdate">MDATE</span>，我的内容</div>' }
       ],
-      //
-      template_cdate_format: '[CDATE: %m/%d/%Y : %H:%M:%S]',
-      template_mdate_format: '[MDATE: %m/%d/%Y : %H:%M:%S]',
-      image_caption: true,
-      /**
-       * 下面方法是为tinymce添加自定义插入图片按钮
-       * 借助iview的Upload组件,将图片先上传到存储云上，再将图片的存储地址放入编辑器内容
-       */
-      setup: (editor) => {
-        editor.ui.registry.addButton('imageUpload', {
-          // text: '插入图片',
-          tooltip: '插入图片',
-          icon: 'image',
-          onAction: () => {
-            let upload = that.$refs.imageUpload as any
-            upload.handleClick()
-          }
-        })
-      }
+      suffix: '.min',
+      modelEvents: 'change keyup undo redo',
+      menubar: true,
+      mediaembed_service_url: '',
+      file_picker_types: 'file image media',
+      valid_children: '+span[div|p]',
+      powerpaste_allow_local_images: false,
+      powerpaste_block_drop: true
     }
   }
   private activated () {
